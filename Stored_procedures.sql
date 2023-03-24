6cc642920aa1c2e381615251efbd602e38321a46
SumanthW <128551121+SumanthW@users.noreply.github.com>
2023-03-23 19:55:29 -0500

    Adding pass recharge trigger

--- Stored_procedures.sql
+++ Stored_procedures.sql
@@ -1,1 +1,23 @@
-
+CREATE OR REPLACE TRIGGER pass_recharge
+    AFTER INSERT ON RECHARGE
+    FOR EACH ROW
+    WHEN (new.recharge_type = 'Pass')
+DECLARE
+    v_Recharge_Value number;
+    v_RECHARGE_ID number;
+    v_CARD_ID number;
+    v_Number_of_days number;
+    v_PASS_TYPE number;
+BEGIN
+    v_Recharge_Value = new.value_of_transaction;
+    v_RECHARGE_ID = new.recharge_id;
+    v_CARD_ID = select distinct card_id from CARD where wallet_id = new.wallet_id;
+    v_PASS_TYPE = select pass_id from PASS_TYPE where price = v_Recharge_Value;
+    v_Number_of_days = select no_of_days from PASS_TYPE where price = v_Recharge_Value;
+    
+    INSERT INTO Pass(card_id,pass_expiry,pass_type_id, recharge_id, valid_from) values (v_CARD_ID,SYSDATE+v_Number_of_days,v_PASS_TYPE,v_RECHARGE_ID,SYSDATE);
+    exception
+        when others then
+            dbms_output.put_line('INVALID TRANSACTION. REVERTING TRANSACTION');
+            delete from RECHARGE where recharge_id = v_RECHARGE_ID;
+END;
